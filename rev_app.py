4ba11e436bbf48681a5eec85e99ee9251ebdc60c
Radan Petrica <radan.petrica@gmail.com>
2021-06-03 20:56:22 +0300

    "repair menu"

--- rev_app.py
+++ rev_app.py
@@ -29,9 +29,11 @@ st.sidebar.header('REVOLUT STOCKS SCANNER')
 st.header('REVOLUT STOCKS LIST')
 
 # Market title web page:
-@st.cache(allow_output_mutation=True)
+# @st.cache(allow_output_mutation=True)
 def stock_list():
-    stock_list = pd.read_excel('stocks_list.xlsx', sheet_name='stocks_list')        
+    stock_list = pd.read_excel('stocks_list.xlsx', sheet_name='stocks_list')
+    stock_list
+    
     return stock_list
 
 
@@ -55,67 +57,67 @@ schedule_time = st.sidebar.number_input(label="Schedule time:",
 ##################################################################################################################################
 
 ##################################################################################################################################
-st.sidebar.subheader('Stock selection')
-# Stock selection
-stock_selection_option = ['Company name',
-                          'Company sector',
-                          'Company industry',
-                          'Revolut stocks',
-                          'Personal portfolio']
+# st.sidebar.subheader('Stock selection')
+# # Stock selection
+# stock_selection_option = ['Company name',
+#                           'Company sector',
+#                           'Company industry',
+#                           'Revolut stocks',
+#                           'Personal portfolio']
 
-stoc_selection_list = st.sidebar.selectbox('Selection mode:', stock_selection_option, key='1')
+# stoc_selection_list = st.sidebar.selectbox('Selection mode:', stock_selection_option, key='1')
 ##################################################################################################################################
 
 ##################################################################################################################################
-def stocks_selections():
-    # View stocks list
-    stocks = stock_list()
-    # Filters
-    stocks_company_name = stocks['Company name'].unique()
-    stocks_company_symbol = stocks['Symbol'].unique()
-    stocks_company_sector = stocks['Sector'].unique()
-    stocks_company_industry = stocks['Industry'].unique()
+# def stocks_selections():
+#     # View stocks list
+#     stocks = stock_list()
+#     # Filters
+#     stocks_company_name = stocks['Company name'].unique()
+#     stocks_company_symbol = stocks['Symbol'].unique()
+#     stocks_company_sector = stocks['Sector'].unique()
+#     stocks_company_industry = stocks['Industry'].unique()
         
-    if stoc_selection_list == 'Company name':    
-        st.text('Stocks selected by name')
-        stocks_filtered_by_name = st.sidebar.multiselect('Company name:', stocks_company_name)
-        if stocks_filtered_by_name:
-            stocks = stocks[(stocks['Company name'].isin(stocks_filtered_by_name))]
-            stocks
-            return stocks
+#     if stoc_selection_list == 'Company name':    
+#         st.text('Stocks selected by name')
+#         stocks_filtered_by_name = st.sidebar.multiselect('Company name:', stocks_company_name)
+#         if stocks_filtered_by_name:
+#             stocks = stocks[(stocks['Company name'].isin(stocks_filtered_by_name))]
+#             stocks
+#             return stocks
         
-    elif stoc_selection_list == 'Company sector':    
-        st.text('Stocks selected by sector')
-        stocks_filtered_by_sector = st.sidebar.multiselect('Company sector:', stocks_company_sector)
-        if stocks_filtered_by_sector:
-            stocks = stocks[(stocks['Sector'].isin(stocks_filtered_by_sector))]
-            stocks 
-            return stocks 
+#     elif stoc_selection_list == 'Company sector':    
+#         st.text('Stocks selected by sector')
+#         stocks_filtered_by_sector = st.sidebar.multiselect('Company sector:', stocks_company_sector)
+#         if stocks_filtered_by_sector:
+#             stocks = stocks[(stocks['Sector'].isin(stocks_filtered_by_sector))]
+#             stocks 
+#             return stocks 
     
-    elif stoc_selection_list == 'Company industry':    
-        st.text('Stocks selected by industry')
-        stocks_filtered_by_industry = st.sidebar.multiselect('Company industry:', stocks_company_industry)
-        if stocks_filtered_by_industry:
-            stocks = stocks[(stocks['Industry'].isin(stocks_filtered_by_industry))]
-            stocks
-            return stocks  
+#     elif stoc_selection_list == 'Company industry':    
+#         st.text('Stocks selected by industry')
+#         stocks_filtered_by_industry = st.sidebar.multiselect('Company industry:', stocks_company_industry)
+#         if stocks_filtered_by_industry:
+#             stocks = stocks[(stocks['Industry'].isin(stocks_filtered_by_industry))]
+#             stocks
+#             return stocks  
     
-    elif stoc_selection_list == 'Revolut stocks':    
-        st.text('All Revolut Stocks')
-        if stoc_selection_list:
-            stocks
-            return stocks
+#     elif stoc_selection_list == 'Revolut stocks':    
+#         st.text('All Revolut Stocks')
+#         if stoc_selection_list:
+#             stocks
+#             return stocks
     
-    elif stoc_selection_list == 'Personal portfolio':    
-        st.text('Personal portfolio Stocks')        
+#     elif stoc_selection_list == 'Personal portfolio':    
+#         st.text('Personal portfolio Stocks')        
             
             
-    else:
-        st.warning('Please load data')
+#     else:
+#         st.warning('Please load data')
         
-    return stocks
+#     return stocks
 
-stocks_list = stocks_selections()
+stocks_list = stock_list()
 #################################################################################################################################
 
 #Tehnical indicators
@@ -176,10 +178,8 @@ def rsi_live():
     company_names = stocks_list['Company name'].values
     company_symbols = stocks_list['Symbol'].values
     for name, symbol in zip(company_names, company_symbols):
-        try:
-            
+        try: 
             stocks_data = yahoo_data(symbol) #Get data
-            
             
             if stocks_data['RSI'][-1] < 50 and stocks_data['RSI'][-1] > 30:
                 stocks_data['recommndation'] = "bearish downtrend"
